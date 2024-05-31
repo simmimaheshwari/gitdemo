4bf8c6f1e95a32bdc7f185c212e1f298737ee255
urvas <urvas@DESKTOP-QDKT7QF>
2024-05-31 19:46:57 +0530

    first commit...

--- src/gitdemoproject/test1.java
+++ src/gitdemoproject/test1.java
@@ -2,9 +2,21 @@ package gitdemoproject;
 
 public class test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	
+	{
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			
+			System.out.println("eat something....");
+		
+		else
+		{
+			
+			System.out.println("Do work....");
+		}	
 	}
 
 }
