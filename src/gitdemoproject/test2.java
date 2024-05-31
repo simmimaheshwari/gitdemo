fa38020650f40e13386b82760bec9ad575d838cf
simmimaheshwari <maheshwarisimmi147@gmail.com>
2024-05-31 21:22:07 +0530

    Add files via upload

---
+++ src/gitdemoproject/test2.java
@@ -0,0 +1,20 @@
+package gitdemoproject;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=5;
+		if(num%2==0)
+		{
+			System.out.println("It is a Even Number");
+		}
+		else
+		{
+			System.out.println("It is a Odd Number");
+	
+		}
+	}
+
+}
